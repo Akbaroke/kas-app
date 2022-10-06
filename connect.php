cd3e06fd4031ff3b9a2e3829c7374cab2fce7873
akbaroke <akbarmuhammad833@gmail.com>
2022-10-06 14:50:37 +0700

    online

--- connect.php
+++ connect.php
@@ -1,3 +1,3 @@
 <?php
 // koneksi database
-$conn = mysqli_connect("localhost", "root", "", "kas-app");
+$conn = mysqli_connect("151.106.118.101", "u1607362_kas-app", "15sep2001", "u1607362_kas-app");
