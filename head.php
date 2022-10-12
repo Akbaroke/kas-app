874c9172e67e28bff15ff3c8dd758e03809de7e7
akbaroke <akbarmuhammad833@gmail.com>
2022-10-12 16:21:11 +0700

    update ukuran layar chatt

--- head.php
+++ head.php
@@ -9,7 +9,7 @@ ob_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="author" content="Akbaroke">
-  
+  <meta name="keywords" content="kas ti.21.a3" >
 
   <!-- sweetalert2 -->
   <link rel="stylesheet" href="cdn/sweetalert2.min.css">
