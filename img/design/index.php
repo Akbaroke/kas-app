c2bb061f28db3a40f916ec2741d083672559f33b
akbaroke <akbarmuhammad833@gmail.com>
2022-10-19 13:55:43 +0700

    update session folder terlarang

---
+++ img/design/index.php
@@ -0,0 +1,8 @@
+<?php
+include "session_cek.php";
+
+// Terlarang !
+if(!isset($_SESSION["ahjsdgashjgdYAGD232y8suhhhdq82047hfdjasda8duy9081qhs uiqhdquhhhhhdq9d0u9YAGD232y8suhy8suhhhdqjasda8duy9081qhs uiqhdquhhhhhdq9d0u90hhdq820477hf2u8w7uye89yihhdq82047hfdhjf290fh28h^%&^%$&(&hhhhdq9d0u90^&hjsdgashjgdYAGD232y8suhhhdq82047hfdjasda8duy9081qhs uiqhdquhhhhhdq9d0u90hhdq82047hf2u8w7uye89yihhdq82047hfdhjf290fh28h^%&^%$&^$&$082047hfsihisdhosd^&2hyidhjf290fhhdq82047hfshhdq82047hfih9d0u902u8w7uye89323#$)^$&(^ashashaihifg82t28hdoqasuhdu^*&^*&^*%(%082047hfsihisdhosd^&2hyidhjf290fhhdq82047hfshhdq82047hfih9d0u902u8w7uye89323#$)^$&(^ashashaihifg82t28hdoqasuhdu"])){
+  header("Location: ../login.php");
+  exit;
+}
