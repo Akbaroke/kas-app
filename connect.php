c1e05702548eaa6bb8a0a7e8d6e4e06181fff5cd
akbaroke <akbarmuhammad833@gmail.com>
2023-02-24 16:27:37 +0700

    update

--- connect.php
+++ connect.php
@@ -1,7 +1,11 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $servername = "localhost";
 $database = "kas-app";
-$username = "root";
-$password = "";
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
